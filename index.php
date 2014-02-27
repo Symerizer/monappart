d5708a8eafc1f7fe823a205380f2a360221aa2b5
Simon Mercier <jackoftrefle@gmail.com>
2014-02-27 02:15:02 -0500

    Initial commit.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MonAppart</title>
+
+    <link rel="stylesheet/less" type="text/css" href="less/bootstrap.less"/>
+</head>
+
+<body>
+
+
+
+
+<script src="lib/jquery-2.1.0.js"></script>
+<script src="lib/less.js"></script>
+</body>
+</html>
